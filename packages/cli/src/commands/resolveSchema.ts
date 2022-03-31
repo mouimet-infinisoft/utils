ed0f4cd8502e1d8d8e66bfda5ca489eb0433315b
Martin Ouimet <mouimet@infini-soft.com>
2022-03-31 06:07:17 -0400

    new command resolveSchema

--- packages/cli/src/commands/resolveSchema.ts
+++ packages/cli/src/commands/resolveSchema.ts
@@ -8,136 +8,79 @@
  */
 
 import axios from 'axios';
+import { writeFileSync } from 'fs';
+import { Context, ParserReturn } from '../internals';
 
-// const removeHeader = (schema: object) => {
-//     let result = {}
-//     try {
-//         const { id, type, title, description, ...rest } = schema as any
-//         result = rest;
-//     } catch (error) {
-//         console.error('Failed!')
-//     }
-
-//     return result
-// }
+const baseUrl = "https://www.kitchen.infini-soft.com/share/schemas"
+const URI = /(?:(?:(?:ftp|http)[s]*:\/\/|www\.)[^\.]+\.[^ \n|"]+)/gi
 
 /**
- * 
+ * Resolves JSON Schema $ref from `baseURL`
  * @param unresolved Unserialized JSON Schema
  * @param resource   REST API Resource
  * @returns          Unserialized Resolved JSON Schema
  */
 const resolve = async (unresolved: string, resource: string) => {
-    // let draftSchema = unresolved;
-    // draftSchema['components'] = {}
-
     // all URL exclusive map
-    const allReferenceUrl = [...new Set(JSON.stringify(unresolved).match(/(?:(?:(?:ftp|http)[s]*:\/\/|www\.)[^\.]+\.[^ \n|"]+)/gi))]
-    // Replace all FQ pattern buy relative Truncate 
-    // https://www.kitchen.infini-soft.com/share/schema/common/list.json
-    // #/components/schema/common/list.json
+    const allReferenceUrl = [...new Set(JSON.stringify(unresolved).match(URI))].filter(url => url.includes(baseUrl))
 
+    let draftSchema = JSON.parse(JSON.stringify(unresolved).replace(/{{{REPLACE}}}/g, resource))
+    const allFetchPromises = await Promise.allSettled(allReferenceUrl.map((e) => axios.get(e)))
+    const allSchemas = allFetchPromises.map((r: any) => r.value.data)
 
-    let draftSchema = JSON.stringify(unresolved).replaceAll("https://www.kitchen.infini-soft.com/share/schema", '#').replaceAll('{{{REPLACE}}}', resource)
-    console.log(`draftSchema`, draftSchema)
-    // console.log(`draftSchema =`, draftSchema)
-    console.log(`allReferenceUrl =`, allReferenceUrl)
+    // Should be replaced with reduce
+    const unused = allSchemas.forEach((fetchedSchema) => {
+        const { $id, $schema, type, description, title, properties, definitions, ...strippedSchema } = fetchedSchema
 
-    // Fetch all schema
-    // const allFetch = await Promise.allSettled([...new Set(JSON.stringify(unresolved).match(/(?:(?:(?:ftp|http)[s]*:\/\/|www\.)[^\.]+\.[^ \n|"]+)/gi))].map(axios.get))
-    const allFetch = await Promise.allSettled(allReferenceUrl.map((e) => axios.get(e)))
-    const allSchemaFetch = allFetch.map((r: any) => {
-        console.log(r.value)
-        return r.value
+        draftSchema['components'] = {
+            schemas: {
+                ...(draftSchema?.['components']?.['schemas'] ?? {}),
+                ...JSON.parse(JSON.stringify(strippedSchema))
+            }
+        }
+
+        // Update local references
+        const schemaKey = Object.keys(strippedSchema)[0]
+        draftSchema = JSON.parse(JSON.stringify(draftSchema).replace(new RegExp($id, "g"), `#/components/schemas/${schemaKey}`))
     })
 
-    // Stack definitions
-    allSchemaFetch.forEach(console.log)
-
-    // Concatente at the end
-    // console.log(`fetched schema `, allSchemaFetch)
-
-    // const unused = (await Promise.allSettled(allSchemaFetch)).forEach(({ value: fetchedSchema }: any) => {
-    //     const { $id, $schema, type, description, title, properties, definitions, ...strippedSchema } = fetchedSchema
-    //     delete fetchedSchema['default']
-    //     // getFilename(fetchedSchema['$id'])
-    //     // console.log(`relative`, relativePath)
-    //     // console.log(`fe6tchshcemae`, fetchedSchema)
-
-    //     console.log(`strippeschema ` + strippedSchema)
-
-    //     draftSchema['components'] = {
-    //         schemas: {
-    //         ...JSON.parse(JSON.stringify(draftSchema['schemas'])),
-    //         ...JSON.parse(JSON.stringify(strippedSchema))
-    //         }
-    //     }
-    //     // console.log(`HJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJBHJB` + JSON.stringify(resolved['components']['schema']))
-    //     // resolved = JSON.parse(JSON.stringify(resolved).replaceAll(JSON.stringify({ "$ref": fetchedSchema['$id'] }), JSON.stringify(fetchedSchema)).replaceAll('{{{REPLACE}}}', resource))
-    // })
-
-
-    // resolved['components'] = {
-    //     ...resolved['components'],
-    //     schema: draftSchema.schema,
-    // }
-
-    // console.log(JSON.stringify(resolved['components']))
-
-    return {
-        ...JSON.parse(draftSchema),
-        ...allSchemaFetch
-    }
+    // recursively resolve references
+    return JSON.stringify(draftSchema).match(URI)?.length > 0 ? resolve(draftSchema, resource) : draftSchema
 }
 
-// const getFilename = (id: string) => {
-//     const parsed = id.split('/')
-//     const file = parsed[parsed.length - 1]
-//     const fileName = file.split('.')[0]
-//     const extension = file.split('.')[1]
-
-//     console.log(`parsed`, parsed)
-//     console.log(`file`, file)
-//     console.log(`fileName`, fileName)
-//     console.log(`extensoiopn`, extension)
-// }
-
-/**
- * 
- * @param unresolved Unserialized JSON Schema
- * @returns          Unserialized Resolved JSON Schema
- */
-// const getRefUrlList = (unresolved: unknown) => [...new Set(JSON.stringify(unresolved).match(/(?:(?:(?:ftp|http)[s]*:\/\/|www\.)[^\.]+\.[^ \n|"]+)/gi))]
+type ResolveSchemaProps = {
+    args: ParserReturn
+    context: Context
+}
 
 /**
  * Command entry point
  */
-export const resolveSchema = async () => {
+export const resolveSchema = async ({ args, context }: ResolveSchemaProps) => {
+    const { logger } = context
+    const { command } = args
 
-    console.log(`
-    Resolve JSON Schema Reference
-    `)
+    logger.info({ message: `Resolve JSON Schema Reference` })
 
     const schemaURL = process.argv[3]
     const resource = process.argv[4]
     const outputFile = process.argv[5]
 
     if (!schemaURL || !resource || !outputFile) {
-        console.log(`Usage: ${process.argv[1]} [schema URL] [REST resource name] [output file]`)
-        console.log(`Usage: ${process.argv[1]} https://www.kitchen.infini-soft.com/share/schema/api/rest.json contact resolved.json`)
+        logger.info({ message: `Usage: ${command} [schema URL] [REST resource name] [output file]` })
+        logger.info({ message: `Usage: ${command} https://www.kitchen.infini-soft.com/share/schemas/api/rest.json contact resolved.json` })
         process.exit()
     }
 
     const schema = await axios.get(schemaURL)
-    // const result = await resolve(schema.data, resource)
-    //@ts-ignore
-    // writeFileSync(outputFile, result)
+    const result = await resolve(schema.data, resource)
+    writeFileSync(outputFile, JSON.stringify(result))
 
-    console.log(`
-    Completed
-    Fetched: ${schemaURL}
-    Schema written to ${outputFile}
-    `)
+    logger.info({
+        message: `
+    Completed`, context: {
+            schemaURL,
+            outputFile,
+        }
+    })
 }
-
-resolveSchema()
